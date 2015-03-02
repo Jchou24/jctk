cbdd2d6ecbfc31ad7bee006e5a260e20f142cf53
Jchou24 <johnny1991524@gmail.com>
2015-03-03 05:16:42 +0800

    update

--- text_processing/__init__.py
+++ text_processing/__init__.py
@@ -10,6 +10,9 @@
 #     chunker:    TagChunker
 #     corpus
 
+import codecs
+import os.path
+
 from normalizer import *
 from tokenizer import *
 from stemmer import *
@@ -17,3 +20,9 @@ from lemmatizer import *
 from pos_tagger import pos_tagger
 from chunker import TagChunker
 from corpus import *
+
+en_stop_words = []
+
+en_stop_words_file = os.path.join( os.path.dirname(__file__), "english.stop" )
+with codecs.open(en_stop_words_file,'r') as op:
+	en_stop_words = [ line.strip() for line in op ]
